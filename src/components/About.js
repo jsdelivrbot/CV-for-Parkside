1df540ba749e9837dd5778aa453ad22893ec54ab
Erol Aliyev <aliyeverol98@gmail.com>
2018-04-20 17:17:50 +0200

    Test

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+      <div className="about__info">
+        <h1>Erol Aliyev</h1>
+        <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</p>
+      </div>
+  );
+}
+
+export default About;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CV from './components/CV';
+import 'normalize.css';
+import './styles/styles.scss';
+
+ReactDOM.render(<CV />, document.getElementById('root'));
