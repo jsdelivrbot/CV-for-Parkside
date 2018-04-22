99f81eac9d553b75bd8f30705a0f166549704b3e
Erol Aliyev <aliyeverol98@gmail.com>
2018-04-22 12:48:47 +0200

    Finish 'About' section

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
-const About = () => {
-  return (
-      <div className="about__info">
-        <h1>Erol Aliyev</h1>
-        <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</p>
-      </div>
-  );
-}
+const About = (props) => (
+  <div className="container" id="top" >
+    <video autoPlay loop muted playsInline className="container__video">
+      <source src="https://d2g3i7yf6pozhs.cloudfront.net/data/videos/parkside-final-v5.mp4" type="video/mp4" />
+    </video>
+    <div className="container__video--overlay" ></div>
+    <div className="container__about">
+      <h2 className="container__about--header">Erol Aliyev</h2>
+      <p className="container__about--text">an enthusiastic frontend developer. I would love an opportunity to work with this dream team at Parkside.</p>
+    </div>
+  </div>
+);
 
 export default About;
