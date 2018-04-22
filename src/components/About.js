c3afc57719c2d652294f87a98ce27cddd0117075
Erol Aliyev <aliyeverol98@gmail.com>
2018-04-22 12:56:01 +0200

    Changed class names to avoid future bugs

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 const About = (props) => (
-  <div className="container" id="top" >
-    <video autoPlay loop muted playsInline className="container__video">
+  <div className="aboutSectionContainer" id="top" >
+    <video autoPlay loop muted playsInline className="aboutSectionContainer__video">
       <source src="https://d2g3i7yf6pozhs.cloudfront.net/data/videos/parkside-final-v5.mp4" type="video/mp4" />
     </video>
-    <div className="container__video--overlay" ></div>
-    <div className="container__about">
-      <h2 className="container__about--header">Erol Aliyev</h2>
-      <p className="container__about--text">an enthusiastic frontend developer. I would love an opportunity to work with this dream team at Parkside.</p>
+    <div className="aboutSectionContainer__video--overlay" ></div>
+    <div className="aboutSectionContainer__about">
+      <h2 className="aboutSectionContainer__about--header">Erol Aliyev</h2>
+      <p className="aboutSectionContainer__about--text">an enthusiastic frontend developer. I would love an opportunity to work with this dream team at Parkside.</p>
     </div>
   </div>
 );
