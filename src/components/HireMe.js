4a5dd83d3db8f810e4222d85d540118f5a1eb4e8
Erol Aliyev <aliyeverol98@gmail.com>
2018-04-23 22:24:10 +0200

    Change class names to follow general convention

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const HireMe = (props) => (
-  <div className="hireMeSection" >
-    <p className="hireMeSection__header" >Hire Me</p>
-    <p className="hireMeSection__info" >
+  <div className="hireMeSectionContainer" >
+    <p className="hireMeSectionContainer__header" >Hire Me</p>
+    <p className="hireMeSectionContainer__info" >
       I enjoy working with and learning from others. I follow the Read, Search, Ask philosophy.
       My passion is front-end development and design and I love to create detail-oriented projects.
     </p>
-    <p className="hireMeSection__info" >Digital Creative Design is my passion, too.</p>
+    <p className="hireMeSectionContainer__info" >Digital Creative Design is my passion, too.</p>
   </div>
 );
 
