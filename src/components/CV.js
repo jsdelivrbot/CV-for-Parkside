a335d7a7e20747bb08d6755a0656e6467c55ef10
Erol Aliyev <aliyeverol98@gmail.com>
2018-04-23 22:02:53 +0200

    Finished 'HireMe' section

--- src/components/CV.js
+++ src/components/CV.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import About from './About';
+import HireMe from './HireMe';
 import Navbar from './Navbar';
 import Skills from './Skills';
 import Work from './Work';
+
+// To do
+// Indicators in Work section
+// Modernizr
+
 
 const CV = (props) => (
   <div>
@@ -10,6 +16,7 @@ const CV = (props) => (
     <About />
     <Skills />
     <Work />
+    <HireMe />
   </div>
 );
 
---
+++ src/components/HireMe.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HireMe = () => (
+  <div className="hireMeSection" >
+    <p className="hireMeSection__header" >Hire Me</p>
+    <p className="hireMeSection__info" >
+      I enjoy working with and learning from others. I follow the Read, Search, Ask philosophy.
+      My passion is front-end development and design and I love to create detail-oriented projects.
+    </p>
+    <p className="hireMeSection__info" >Digital Creative Design is my passion, too.</p>
+  </div>
+);
+
+export default HireMe;
