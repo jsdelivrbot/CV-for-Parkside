b5769a3e7687b1225aeb423207245ed8c3bade00
Erol Aliyev <aliyeverol98@gmail.com>
2018-04-24 13:25:09 +0200

    Finished 'Footer' section

--- src/components/CV.js
+++ src/components/CV.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import About from './About';
 import Contact from './Contact';
+import Footer from './Footer';
 import HireMe from './HireMe';
 import Navbar from './Navbar';
 import Skills from './Skills';
 import Work from './Work';
 
 // To do
-// Current section highlighted in navbar
-// Indicators in Work section
+// Current section highlighted in navbar - Not gonna do
+// Indicators in Work section - Not gonna do
+// Test - How?
+// Create settings.scss
+// Production ready (especially webpack) - How?
 // Modernizr
 
 
@@ -20,6 +24,7 @@ const CV = (props) => (
     <Work />
     <HireMe />
     <Contact />
+    <Footer />
   </div>
 );
 
---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="footerSectionContainer" >
+    <p>Think Interactive First.</p>
+    <ul className="footerSectionContainer__list" >
+      <li>
+        <a href="#top" className="footerSectionContainer__listItem" >About</a>
+      </li>
+      <li>
+        <a href="#skills" className="footerSectionContainer__listItem" >Skills</a>
+      </li>
+      <li>
+        <a href="#work" className="footerSectionContainer__listItem" >Work</a>
+      </li>
+      <li>
+        <a href="#contact" className="footerSectionContainer__listItem" >Contact</a>
+      </li>
+    </ul>
+  </div>
+);
+
+export default Footer;
