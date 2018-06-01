53819302864093271534c59c354bb3e9f2bc692c
Erol Aliyev <aliyeverol98@gmail.com>
2018-06-01 09:57:23 +0200

    Update HireMe.js

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -7,7 +7,7 @@ const HireMe = (props) => (
       I enjoy working with and learning from others. I follow the Read, Search, Ask philosophy.
       My passion is front-end development and design and I love to create detail-oriented projects.
     </p>
-    <p className="hireMeSectionContainer__info" >Digital Creative Design is my passion, too.</p>
+    <p className="hireMeSectionContainer__info" >My heart beats digitally, too.</p>
   </div>
 );
 
